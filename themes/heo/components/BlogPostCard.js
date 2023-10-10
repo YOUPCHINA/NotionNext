7c47196f8d23c21f0c85fa956d43bd9488145b3f
YOUPCHINA <147316898+YOUPCHINA@users.noreply.github.com>
2023-10-10 16:28:03 +0800

    Update BlogPostCard.js

--- themes/heo/components/BlogPostCard.js
+++ themes/heo/components/BlogPostCard.js
@@ -18,7 +18,7 @@ const BlogPostCard = ({ index, post, showSummary, siteInfo }) => {
                 data-aos-duration="200"
                 data-aos-once="false"
                 data-aos-anchor-placement="top-bottom"
-                className={'border bg-white dark:bg-[#1e1e1e] flex mb-4 flex-col h-[23rem] md:h-52 md:flex-row 2xl:h-96 2xl:flex-col group w-full dark:border-gray-600 hover:border-indigo-600  dark:hover:border-yellow-600 duration-300 transition-colors justify-between overflow-hidden rounded-xl'}>
+                className={'border bg-white dark:bg-[#1e1e1e] flex mb-4 flex-col h-[23rem] md:h-52 md:flex-row 2xl:h-96 2xl:flex-col group w-full dark:border-gray-50 hover:border-gray-900  dark:hover:border-green-400 duration-300 transition-colors justify-between overflow-hidden rounded-xl'}>
 
                 {/* 图片封面 */}
                 {showPageCover && (
@@ -33,9 +33,9 @@ const BlogPostCard = ({ index, post, showSummary, siteInfo }) => {
                 <div className={'flex p-6 2xl:p-4 flex-col justify-between h-48 md:h-full 2xl:h-48 w-full md:w-7/12 2xl:w-full'}>
                     <div>
                         {/* 分类 */}
-                        {post?.category && <div className={`flex mb-1 items-center ${showPreview ? 'justify-center' : 'justify-start'} hidden md:block flex-wrap dark:text-gray-500 text-gray-600 `}>
+                        {post?.category && <div className={`flex mb-1 items-center ${showPreview ? 'justify-center' : 'justify-start'} hidden md:block flex-wrap dark:text-gray-50 text-gray-50 `}>
                             <Link passHref href={`/category/${post.category}`}
-                                className="cursor-pointer text-xs font-normal menu-link hover:text-indigo-700 dark:hover:text-yellow-700  dark:text-gray-600 transform">
+                                className="cursor-pointer text-xs font-normal menu-link hover:text-red-700 dark:hover:text-green-400  dark:text-gray-50 transform">
                                 {post.category}
                             </Link>
                         </div>}
@@ -44,14 +44,14 @@ const BlogPostCard = ({ index, post, showSummary, siteInfo }) => {
                         <Link
                             href={`${BLOG.SUB_PATH}/${post.slug}`}
                             passHref
-                            className={' group-hover:text-indigo-700 dark:hover:text-yellow-700 dark:group-hover:text-yellow-600 text-black dark:text-gray-100  line-clamp-2 replace cursor-pointer text-xl font-extrabold leading-tight'}>
+                            className={' group-hover:text-green-400 dark:hover:text-green-400 dark:group-hover:text-green-400 text-black dark:text-gray-50  line-clamp-2 replace cursor-pointer text-xl font-extrabold leading-tight'}>
                             <span className='menu-link '>{post.title}</span>
                         </Link>
                     </div>
 
                     {/* 摘要 */}
                     {(!showPreview || showSummary) && (
-                        <p className="line-clamp-2 replace my-3 2xl:my-1 text-gray-700  dark:text-gray-300 text-sm font-light leading-tight">
+                        <p className="line-clamp-2 replace my-3 2xl:my-1 text-gray-900  dark:text-gray-50 text-sm font-light leading-tight">
                             {post.summary}
                         </p>
                     )}
