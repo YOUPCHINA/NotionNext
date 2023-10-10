ab4eca1ec55be25694f93505e2ac21f91863a822
YOUPCHINA <147316898+YOUPCHINA@users.noreply.github.com>
2023-10-10 16:29:21 +0800

    Update TouchMeCard.js

--- themes/heo/components/TouchMeCard.js
+++ themes/heo/components/TouchMeCard.js
@@ -12,15 +12,15 @@ export default function TouchMeCard() {
     return <></>
   }
   return (
-        <div className={'relative h-28 text-white flex flex-col'}>
+        <div className={'relative h-28 text-black flex flex-col'}>
 
             <FlipCard
-                className='cursor-pointer lg:p-6 p-4 border rounded-xl bg-[#4f65f0] dark:bg-yellow-600 dark:border-gray-600'
+                className='cursor-pointer lg:p-6 p-4 border rounded-xl bg-[#eeecfc] dark:bg-[#ffffff] dark:border-green-400'
                 frontContent={
                     <div className='h-full'>
                         <h2 className='font-[1000] text-3xl'>{CONFIG.SOCIAL_CARD_TITLE_1}</h2>
                         <h3 className='pt-2'>{CONFIG.SOCIAL_CARD_TITLE_2}</h3>
-                        <div className='absolute left-0 top-0 w-full h-full' style={{ background: 'url(https://bu.dusays.com/2023/05/16/64633c4cd36a9.png) center center no-repeat' }}></div>
+                        <div className='absolute left-0 top-0 w-full h-full' style={{ background: 'url(https://s1.ax1x.com/2023/08/20/pP8QPLF.png) center center no-repeat' }}></div>
                     </div>}
                 backContent={<Link href={CONFIG.SOCIAL_CARD_URL}>
                     <div className='font-[1000] text-xl h-full'>
